7e318cda77524b8f7da99d164e48f56f463ae2cd
kirakiray <pikay@foxmail.com>
2019-09-15 23:25:54 +0800

    update xdframe

--- js/comps/ki-article-list/ki-article-list.js
+++ js/comps/ki-article-list/ki-article-list.js
@@ -1,8 +1,38 @@
 Component({
     tag: "ki-article-list",
-    link: false,
     useComps: ["../ki-ul -pack"],
-    inited() {
-        // debugger
+    proto: {
+        // 更新列表数据
+        refreshActive() {
+            let target = this._target.ele;
+
+            // 判断当前的滚动值
+            let scrollTop = target.scrollTop;
+            let scrollHeight = target.scrollHeight;
+            
+            debugger
+        }
+    },
+    watch: {
+        for(e, val) {
+            // 获取元素
+            let targetEle = $(val);
+
+            // 获取所有标题
+            let titles = targetEle.queAll('h1,h2,h3,h4,h5,h6');
+
+            // 清空列表并填充新元素
+            this.$kiul.empty();
+
+            titles.forEach(e => {
+                this.$kiul.push(`<a>${e.text}</a>`);
+            });
+
+            // 挂载_target
+            this._target = targetEle;
+
+            // 刷新装填
+            this.refreshActive();
+        }
     }
 });
